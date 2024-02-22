192233ccec13f60e439d08387d7802dca6ea17da
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2024-02-22 19:10:04 +0100

    changed some cases on the router

--- js/index.js
+++ js/index.js
@@ -10,18 +10,21 @@ async function router() {
 
   switch (pathname) {
     case "/products/kids/kids.html":
+    case "/products/kids/kids":
       // const filteredKidsProducts = products.filter((k) => k.category === "kids");
       displayProductList(18);
       handleCartButton();
       return;
 
     case "/products/men/men.html":
+    case "/products/men/men":
       // const filteredMenProducts = products.filter((m) => m.category === "men");
       displayProductList(16);
       handleCartButton();
       return;
 
     case "/products/women/women.html":
+    case "/products/women/women":
       // const filteredWomenProducts = products.filter((w) => w.category === "women");
       displayProductList(17);
       handleCartButton();
